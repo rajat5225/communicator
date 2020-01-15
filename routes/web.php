af69908c3a414a517c7178894e180dccf03791a6
anuragsharma2010 <anuragsharma2010@gmail.com>
2020-01-15 18:11:38 +0530

    user role and permission module - 15/10/2020 6:11

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,32 @@
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
+});*/
+
+/*Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');*/
+
+
+Auth::routes();
+
+
+//Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
+
+
+Route::group(['middleware' => ['auth']], function() {
+
+    Route::resource('roles','RoleController');
+
+    Route::resource('users','UserController');
+
+    Route::resource('products','ProductController');
+
 });
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
